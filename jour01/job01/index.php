7f6f63f614c0d3f05cdd2c2d356a806a405ea162
root <root@debian>
2022-11-14 15:04:15 +0100

    job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,33 @@
+<?php
+
+// Création de 3 variables contenant les chaînes de caractères: Vive LaPlateforme !
+$str = "LaPlateforme ";
+$str1 = "Vive ";
+$str2 = "!";
+
+echo $str1;
+echo $str;
+echo $str2;
+
+// Saut à la ligne
+echo "<br>";
+
+// Création d'une variable contenant la valeur numérique 6.
+$val = 6;
+
+echo $val;
+echo "<br>";
+
+// On ajoute 4 à la variable contenant 6
+echo $val+4;
+echo "<br>";
+
+
+$myBool = "true";
+echo $myBool;
+echo "<br>";
+
+$myBool = "false";
+echo $myBool
+
+?>

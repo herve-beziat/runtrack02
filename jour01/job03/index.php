43314fb5184d06287906b242c74131d0d65d92f6
root <root@debian>
2022-11-14 16:38:08 +0100

    job03

---
+++ jour01/job03/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$type = ["Boolean","Entier","Str","Float"];
+$nom = ["var0","var1","var2","var3"];
+$valeur = ["True","12","abgherjy","10.5"];
+
+echo "
+<table border=5>
+    <tr>
+        <th>Type</th>
+        <th>Nom</th>
+        <th>Valeur</th>
+    </tr>
+    <tr>
+        <td>$type[0]</td>
+        <td>$nom[0]</td>
+        <td>$valeur[0]</td>
+    </tr>
+    <tr>
+        <td>$type[1]</td>
+        <td>$nom[1]</td>
+        <td>$valeur[1]</td>
+    </tr>
+    <tr>
+        <td>$type[2]</td>
+        <td>$nom[2]</td>
+        <td>$valeur[2]</td>
+    </tr>
+    <tr>
+        <td>$type[3]</td>
+        <td>$nom[3]</td>
+        <td>$valeur[3]</td>
+    </tr>
+</table>    
+"
+?>
